90ab0bbebbb2979622b624327d49e2234cc6ed3a
sarahanduca <anduca.sarah@gmail.com>
2023-08-14 13:59:33 -0300

    fix evaluate

--- IA/minimax.py
+++ IA/minimax.py
@@ -1,41 +1,46 @@
 import math
 import chess
 
+
 def get_piece_value(piece, maximize_color):
+    # precisa retornar o valor dependendo da cor
+    piece_color = piece.color if maximize_color == 'W' else not piece.color
     if piece.piece_type == 1:
-        return 10 if piece.color == maximize_color else -10
+        return 10 if piece_color == maximize_color else -10
     elif piece.piece_type == 2:
-        return 30 if piece.color == maximize_color else -30
+        return 30 if piece_color == maximize_color else -30
     elif piece.piece_type == 3:
-        return 30 if piece.color == maximize_color else -30
+        return 30 if piece_color == maximize_color else -30
     elif piece.piece_type == 4:
-        return 50 if piece.color == maximize_color else -50
+        return 50 if piece_color == maximize_color else -50
     elif piece.piece_type == 5:
-        return 90 if piece.color == maximize_color else -90
+        return 90 if piece_color == maximize_color else -90
     elif piece.piece_type == 6:
-        return 900 if piece.color == maximize_color else -900
+        return 900 if piece_color == maximize_color else -900
     else:
         return 0
+
 
 def evaluate(board, maximize_color):
     value = 0
     for i in range(64):
         piece = board.piece_at(i)
         if piece is not None:
-            value += abs(get_piece_value(piece, maximize_color))
-    return value if maximize_color else value * -1
-    
+            value += get_piece_value(piece, maximize_color)
+    return value
 
-def minimax(board, depth, alpha, beta, maximize_player):
+
+def minimax(board, depth, alpha, beta, maximize_player, maximize_color):
     if (depth == 0 or board.is_game_over()):
-        return None, evaluate(board, maximize_player)
+        return None, evaluate(board, maximize_color)
     best_move = None
     if maximize_player:
         best_value = -math.inf
         for move in board.legal_moves:
             mv = chess.Move.from_uci(str(move))
             board.push(mv)
-            value = minimax(board, depth - 1, alpha, beta, False)[1]
+            value = minimax(board, depth - 1, alpha, beta,
+                            False, maximize_color)[1]
             board.pop()
             if value > best_value:
                 best_value = value
@@ -49,7 +54,8 @@ def minimax(board, depth, alpha, beta, maximize_player):
         for move in board.legal_moves:
             mv = chess.Move.from_uci(str(move))
             board.push(mv)
-            value = minimax(board, depth - 1, alpha, beta, True)[1]
+            value = minimax(board, depth - 1, alpha,
+                            beta, True, maximize_color)[1]
             board.pop()
             if value < best_value:
                 best_value = value
--- chessMain.py
+++ chessMain.py
@@ -1,5 +1,6 @@
 import chess
-import minimax
+import chess.svg
+import IA.minimax as minimax
 import math
 import random
 import sys
@@ -7,15 +8,47 @@ import sys
 MIN, MAX = -100000, 100000
 
 
+def display_text_board(board):
+    # Create a mapping for file and rank names
+    file_names = ' abcdefgh'
+    rank_names = ' 12345678'
+
+    # Print the column headers (files)
+    # print('   ' + '  '.join(file_names[1:9]))
+
+    # Loop through ranks in reverse order
+    for rank in range(7, -1, -1):
+        row = []
+        # Loop through files
+        for file in range(8):
+            square = chess.square(file, rank)
+            piece = board.piece_at(square)
+            if piece:
+                row.append(piece.symbol())
+            else:
+                row.append(' ')
+        # Print rank name and row
+        print(rank_names[rank + 1] + ' | ' + ' | '.join(row) + ' | ')
+        print('  ---------------------------------')
+
+    # Print the column headers (files) again at the bottom
+    print('    ' + '   '.join(file_names[1:9]))
+
+
 def main():
     board = chess.Board()
     turn = "White"
     player = []
     ai = []
+    count = 0
+
     while not board.is_game_over():
         legal_moves = True
-        print('--------------------------------------------')
-        print(board)
+        print('Jogada', count)
+        count += 1
+        # You can set any desired position here
+        display_text_board(board)
+        # print(board)
         if turn == "White":
             print("Movimentos Permitidos: ", board.legal_moves)
             while legal_moves:
@@ -23,13 +56,13 @@ def main():
                 if chess.Move.from_uci(move) in board.legal_moves:
                     move = chess.Move.from_uci(str(move))
                     legal_moves = False
-                else: 
+                else:
                     print("Movimento inválido")
             player.append(move)
             board.push(move)
             turn = "Black"
         else:
-            move, score = minimax.minimax(board, 4, MIN, MAX, True)
+            move, score = minimax.minimax(board, 4, MIN, MAX, True, 'B')
             print(move, score)
             board.push(move)
             turn = "White"
@@ -45,6 +78,7 @@ def main():
         print("Draw!")
     writeOnFile(player, ai, outcomeText)
 
+
 def writeOnFile(player, ia, outcomeText):
     file = open("Jogadas.txt", "w")
     file.write("Player: ")
@@ -57,6 +91,7 @@ def writeOnFile(player, ia, outcomeText):
     file.write("\n")
     file.write("Resultado: " + outcomeText)
     file.close()
+
 
 if __name__ == "__main__":
     main()
